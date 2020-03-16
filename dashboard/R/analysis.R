a2e694047bf04d8ddd33aee6a690faad06dae1a6
paulapivat <paul.apivat@gmail.com>
2020-03-16 11:28:32 +0700

    add new files, add changes to new_cases World and Thailand

--- dashboard/R/analysis.R
+++ dashboard/R/analysis.R
@@ -13,6 +13,20 @@ full_data <- read_csv("/Users/paulapivat/Desktop/covid/dashboard/full_data.csv")
 new_cases_thailand <- new_cases %>%
 + select(date, World, Thailand)
 
+## add new columns
+new_cases_thailand[,"World_Changes"] <- NA
+new_cases_thailand[,"Thailand_Changes"] <- NA
+
+## subtract value from previous row using dplyr
+# World
+new_cases_thailand <- new_cases_thailand %>%
++ arrange(date) %>%
++ mutate(World_Changes = World - lag(World, default = first(World)))
+
+# Thailand
+new_cases_thailand <- new_cases_thailand %>%
++ arrange(date) %>%
++ mutate(Thailand_Changes = Thailand - lag(Thailand, default = first(Thailand)))
 
 
 
