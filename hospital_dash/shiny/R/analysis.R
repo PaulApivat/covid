81a8b0c08f3d83bd91dc7f983dfbf42f45048d30
paulapivat <paul.apivat@gmail.com>
2020-04-14 11:28:53 +0700

    add R directory with analysis.R to capture notes on creating Shiny App

---
+++ hospital_dash/shiny/R/analysis.R
@@ -0,0 +1,22 @@
+## notes on creating a Shiny App ##
+
+## Bare minimum of a Shiny App
+
+library(shiny)
+
+# Define UI  ----
+ui <- fluidPage(
+
+  
+)
+
+# Define server logic  ----
+server <- function(input, output) {
+
+
+}
+
+# Run the app ----
+
+shinyApp(ui = ui, server = server)
+
