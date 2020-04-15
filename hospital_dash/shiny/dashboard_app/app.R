2d1e857789b8a5020ebc172969f4b2fc2ae63d3f
paulapivat <paul.apivat@gmail.com>
2020-04-15 20:07:21 +0700

    change font size of section headings

--- hospital_dash/shiny/dashboard_app/app.R
+++ hospital_dash/shiny/dashboard_app/app.R
@@ -26,7 +26,7 @@ ui <- fluidPage(
         
         
         fluidRow(
-          column(6, h1("Patients"), 
+          column(6, h4("Patients"), 
                  numericInput("num_total_patients", "Number of Total Patients", value = 36), 
                  numericInput("num_er_patients", "ER Patients", value = 2),
                  numericInput("num_icu_patients", "ICU Patients", value = 4),
@@ -34,7 +34,7 @@ ui <- fluidPage(
                  numericInput("num_ward_patients", "WARD Patients", value = 10),
                  numericInput("num_dc_patients", "D/C", value = 0),
                  ),
-          column(6, h1("Supplies"),
+          column(6, h4("Supplies"),
                  numericInput("num_total_beds_occupied", "Total Beds Occupied (%)", value = 74),
                  numericInput("num_n95", "N95 Masks", value = 30),
                  numericInput("num_goggles", "Goggles", value = 25),
@@ -47,13 +47,13 @@ ui <- fluidPage(
         extendShinyjs(text = jsCode),
         
         fluidRow(
-          column(6, h1("Beds"),
+          column(6, h4("Beds"),
                  numericInput("num_er_beds", "ER Beds", value = 46, min = 0, step = 1), 
                  numericInput("num_icu_beds", "ICU Beds", value = 50, min = 0, step = 1),
                  numericInput("num_sdu_beds", "SDU Beds", value = 63, min = 0, step = 1),
                  numericInput("num_ward_beds", "WARD Beds", value = 97, min = 0, step = 1),
                  ),
-          column(6, h1("Nurses"),
+          column(6, h4("Nurses"),
                  numericInput("num_er_nurses", "ER Nurses", value = 68, min = 0, step = 1), 
                  numericInput("num_icu_nurses", "ICU Nurses", value = 32, min = 0, step = 1),
                  numericInput("num_sdu_nurses", "SDU Nurses", value = 18, min = 0, step = 1),
@@ -61,7 +61,7 @@ ui <- fluidPage(
                  )
         ),
         fluidRow(
-          column(12, h1("Time"),
+          column(12, h4("Time"),
                  textInput("avg_door_doc", "Average Door to Doctor", value = "83 mins"),
                  textInput("avg_er_floor", "Average ER to Floor", value = "45 mins"),
                  textInput("bed_to_icu", "Bed Transfer to ICU", value = "0 hr 29 mins"),
@@ -74,66 +74,66 @@ ui <- fluidPage(
     
     
     mainPanel(
-      "Main Panel",
+      #"Main Panel",
       fluidRow(
-        column(4, h4("Column1"),
+        column(4, #h3("Column1"),
                
                fluidRow(
-                 column(12, "Patient Section",
+                 column(12, h2("Patients"),
                         fixedRow(
-                            column(4, "Total", verbatimTextOutput("num_total_patients"), tags$head(tags$style(HTML("#num_total_patients {font-size: 36px}")))), 
-                            column(4, "Dept", verbatimTextOutput("num_er_patients"), tags$head(tags$style(HTML("#num_er_patients {background-color: black}", "#num_er_patients {color: white}"))),
+                            column(4, "TOTAL", verbatimTextOutput("num_total_patients"), tags$head(tags$style(HTML("#num_total_patients {font-size: 36px}")))), 
+                            column(4, "DEPARTMENT", verbatimTextOutput("num_er_patients"), tags$head(tags$style(HTML("#num_er_patients {background-color: black}", "#num_er_patients {color: white}"))),
                                               verbatimTextOutput("num_icu_patients"), tags$head(tags$style(HTML("#num_icu_patients {background-color: #505050}", "#num_icu_patients {color: white}"))),
                                               verbatimTextOutput("num_sdu_patients"), tags$head(tags$style(HTML("#num_sdu_patients {background-color: #808080}", "#num_sdu_patients {color: white}"))),
                                               verbatimTextOutput("num_ward_patients"), tags$head(tags$style(HTML("#num_ward_patients {background-color: #D3D3D3}", "#num_ward_patients {color: white}"))),
                                               verbatimTextOutput("num_dc_patients")),
-                            column(4, "Status")
+                            column(4, "STATUS")
                                 )
                        )
                  ),
                
-               fluidRow(column(12, "Total Beds Occupied", verbatimTextOutput("num_total_beds_occupied"), 
+               fluidRow(column(12, h2("Beds Occupied (%)"), verbatimTextOutput("num_total_beds_occupied"), 
                                tags$head(tags$style(HTML("#num_total_beds_occupied {background-color: orange}", "#num_total_beds_occupied {color: black}", "#num_total_beds_occupied {font-size: 36px}"))))),
                
                fluidRow(
-                 column(12, "Supplies",
-                        fixedRow(column(4, "Supplies1", verbatimTextOutput("num_n95"), tags$head(tags$style(HTML("#num_n95 {background-color: green}", "#num_n95 {color: white}", "#num_n95 {font-size: 30px}"))), 
-                                                        verbatimTextOutput("num_ppe"), tags$head(tags$style(HTML("#num_ppe {background-color: green}", "#num_ppe {color: white}", "#num_ppe {font-size: 30px}")))), 
-                                 column(4, "Supplies2", verbatimTextOutput("num_goggles"), tags$head(tags$style(HTML("#num_goggles {background-color: red}", "#num_goggles {color: white}", "#num_goggles {font-size: 30px}"))),
-                                                        verbatimTextOutput("num_vent"), tags$head(tags$style(HTML("#num_vent {background-color: green}", "#num_vent {color: white}", "#num_vent {font-size: 30px}")))), 
-                                 column(4, "Supplies3", verbatimTextOutput("num_gloves"), tags$head(tags$style(HTML("#num_gloves {background-color: orange}", "#num_gloves {color: white}", "#num_gloves {font-size: 30px}"))))
+                 column(12, h2("Supplies"),
+                        fixedRow(column(4, "N95 MASK", verbatimTextOutput("num_n95"), tags$head(tags$style(HTML("#num_n95 {background-color: green}", "#num_n95 {color: white}", "#num_n95 {font-size: 30px}"))), 
+                                            "PPE"     ,  verbatimTextOutput("num_ppe"), tags$head(tags$style(HTML("#num_ppe {background-color: green}", "#num_ppe {color: white}", "#num_ppe {font-size: 30px}")))), 
+                                 column(4, "GOGGLES", verbatimTextOutput("num_goggles"), tags$head(tags$style(HTML("#num_goggles {background-color: red}", "#num_goggles {color: white}", "#num_goggles {font-size: 30px}"))),
+                                          "VENTILATORS", verbatimTextOutput("num_vent"), tags$head(tags$style(HTML("#num_vent {background-color: green}", "#num_vent {color: white}", "#num_vent {font-size: 30px}")))), 
+                                 column(4, "GLOVES", verbatimTextOutput("num_gloves"), tags$head(tags$style(HTML("#num_gloves {background-color: orange}", "#num_gloves {color: white}", "#num_gloves {font-size: 30px}"))))
                                  )
                         )
                  )
                
                ),
-        column(4, h4("Column2"),
-               fluidRow(column(12, "Beds Section",
-                              fixedRow(column(6, "Bed1", verbatimTextOutput("num_er_beds"), tags$head(tags$style(HTML("#num_er_beds {background-color: grey}", "#num_er_beds {color: white}", "#num_er_beds {font-size: 36px}"))),
-                                                         verbatimTextOutput("num_sdu_beds"), tags$head(tags$style(HTML("#num_sdu_beds {background-color: grey}", "#num_sdu_beds {color: white}", "#num_sdu_beds {font-size: 36px}")))
+        column(4, #h3("Column2"),
+               fluidRow(column(12, h2("Beds"),
+                              fixedRow(column(6, "ER", verbatimTextOutput("num_er_beds"), tags$head(tags$style(HTML("#num_er_beds {background-color: grey}", "#num_er_beds {color: white}", "#num_er_beds {font-size: 36px}"))),
+                                                  "SDU", verbatimTextOutput("num_sdu_beds"), tags$head(tags$style(HTML("#num_sdu_beds {background-color: grey}", "#num_sdu_beds {color: white}", "#num_sdu_beds {font-size: 36px}")))
                                               ), 
-                                       column(6, "Bed2", verbatimTextOutput("num_icu_beds"), tags$head(tags$style(HTML("#num_icu_beds {background-color: grey}", "#num_icu_beds {color: white}", "#num_icu_beds {font-size: 36px}"))),
-                                                          verbatimTextOutput("num_ward_beds"), tags$head(tags$style(HTML("#num_ward_beds {background-color: grey}", "#num_ward_beds {color: white}", "#num_ward_beds {font-size: 36px}")))
+                                       column(6, "ICU", verbatimTextOutput("num_icu_beds"), tags$head(tags$style(HTML("#num_icu_beds {background-color: grey}", "#num_icu_beds {color: white}", "#num_icu_beds {font-size: 36px}"))),
+                                                  "WARD", verbatimTextOutput("num_ward_beds"), tags$head(tags$style(HTML("#num_ward_beds {background-color: grey}", "#num_ward_beds {color: white}", "#num_ward_beds {font-size: 36px}")))
                                               )
                                        )
                                )),
-               fluidRow(column(12, "Nurses Section",
-                               fixedRow(column(6, "Nurse1", verbatimTextOutput("num_er_nurses"), tags$head(tags$style(HTML("#num_er_nurses {background-color: grey}", "#num_er_nurses {color: white}", "#num_er_nurses {font-size: 36px}"))),
-                                                            verbatimTextOutput("num_sdu_nurses"), tags$head(tags$style(HTML("#num_sdu_nurses {background-color: grey}", "#num_sdu_nurses {color: white}", "#num_sdu_nurses {font-size: 36px}")))
+               fluidRow(column(12, h2("Nurses"),
+                               fixedRow(column(6, "ER", verbatimTextOutput("num_er_nurses"), tags$head(tags$style(HTML("#num_er_nurses {background-color: grey}", "#num_er_nurses {color: white}", "#num_er_nurses {font-size: 36px}"))),
+                                                  "SDU", verbatimTextOutput("num_sdu_nurses"), tags$head(tags$style(HTML("#num_sdu_nurses {background-color: grey}", "#num_sdu_nurses {color: white}", "#num_sdu_nurses {font-size: 36px}")))
                                                ), 
-                                        column(6, "Nurse2", verbatimTextOutput("num_icu_nurses"), tags$head(tags$style(HTML("#num_icu_nurses {background-color: grey}", "#num_icu_nurses {color: white}", "#num_icu_nurses {font-size: 36px}"))),
-                                                            verbatimTextOutput("num_ward_nurses"), tags$head(tags$style(HTML("#num_ward_nurses {background-color: grey}", "#num_ward_nurses {color: white}", "#num_ward_nurses {font-size: 36px}"))),
+                                        column(6, "ICU", verbatimTextOutput("num_icu_nurses"), tags$head(tags$style(HTML("#num_icu_nurses {background-color: grey}", "#num_icu_nurses {color: white}", "#num_icu_nurses {font-size: 36px}"))),
+                                                  "WARD", verbatimTextOutput("num_ward_nurses"), tags$head(tags$style(HTML("#num_ward_nurses {background-color: grey}", "#num_ward_nurses {color: white}", "#num_ward_nurses {font-size: 36px}"))),
                                                )
                                         )
                                )),
                ),
-        column(4, h4("Column3"),
-               fluidRow(column(12, "Avg Door to Doctor", verbatimTextOutput("avg_door_doc"), tags$head(tags$style(HTML("#avg_door_doc {background-color: orange}", "#avg_door_doc {color: white}", "#avg_door_doc {font-size: 28px}"))))),
-               fluidRow(column(12, "Avg ER to Floor", verbatimTextOutput("avg_er_floor"), tags$head(tags$style(HTML("#avg_er_floor {background-color: orange}", "#avg_er_floor {color: white}", "#avg_er_floor {font-size: 28px}"))))),
-               fluidRow(column(12, "Beds Transfer to ICU", verbatimTextOutput("bed_to_icu"), tags$head(tags$style(HTML("#bed_to_icu {background-color: green}", "#bed_to_icu {color: white}", "#bed_to_icu {font-size: 28px}"))))),
-               fluidRow(column(12, "Beds Transfer to SDU", verbatimTextOutput("bed_to_sdu"), tags$head(tags$style(HTML("#bed_to_sdu {background-color: green}", "#bed_to_sdu {color: white}", "#bed_to_sdu {font-size: 28px}"))))),
-               fluidRow(column(12, "Beds Transfer to WARD", verbatimTextOutput("bed_to_ward"), tags$head(tags$style(HTML("#bed_to_ward {background-color: red}", "#bed_to_ward {color: white}", "#bed_to_ward {font-size: 28px}"))))),
-               fluidRow(column(12, "Beds Time D/C Order to Actual D/C", verbatimTextOutput("dc_order_actual"), tags$head(tags$style(HTML("#dc_order_actual {background-color: orange}", "#dc_order_actual {color: white}", "#dc_order_actual {font-size: 28px}"))))),
+        column(4, h2("Time"),#h3("Column3"), 
+               fluidRow(column(12, "AVG DOOR to DOCTOR", verbatimTextOutput("avg_door_doc"), tags$head(tags$style(HTML("#avg_door_doc {background-color: orange}", "#avg_door_doc {color: white}", "#avg_door_doc {font-size: 28px}"))))),
+               fluidRow(column(12, "AVG ER to FLOOR", verbatimTextOutput("avg_er_floor"), tags$head(tags$style(HTML("#avg_er_floor {background-color: orange}", "#avg_er_floor {color: white}", "#avg_er_floor {font-size: 28px}"))))),
+               fluidRow(column(12, "BED TRANSFER to ICU", verbatimTextOutput("bed_to_icu"), tags$head(tags$style(HTML("#bed_to_icu {background-color: green}", "#bed_to_icu {color: white}", "#bed_to_icu {font-size: 28px}"))))),
+               fluidRow(column(12, "BED TRANSFER to SDU", verbatimTextOutput("bed_to_sdu"), tags$head(tags$style(HTML("#bed_to_sdu {background-color: green}", "#bed_to_sdu {color: white}", "#bed_to_sdu {font-size: 28px}"))))),
+               fluidRow(column(12, "BED TRANSFER to WARD", verbatimTextOutput("bed_to_ward"), tags$head(tags$style(HTML("#bed_to_ward {background-color: red}", "#bed_to_ward {color: white}", "#bed_to_ward {font-size: 28px}"))))),
+               fluidRow(column(12, "D/C ORDER to ACTUAL D/C", verbatimTextOutput("dc_order_actual"), tags$head(tags$style(HTML("#dc_order_actual {background-color: orange}", "#dc_order_actual {color: white}", "#dc_order_actual {font-size: 28px}"))))),
                )
       )
       #plotOutput("distPlot")
