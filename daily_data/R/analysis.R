f6ca818e6eb895e5c723d8a632b87bf772146d36
paulapivat <paul.apivat@gmail.com>
2020-04-19 19:50:23 +0700

    add April 19th data

--- daily_data/R/analysis.R
+++ daily_data/R/analysis.R
@@ -234,6 +234,10 @@ standard_data[84,] <- list('2020-04-13', 'Thailand', 28, 2, 2579, 40, 34786)
 standard_data[85,] <- list('2020-04-14', 'Thailand', 34, 1, 2613, 41, 35452)
 standard_data[86,] <- list('2020-04-15', 'Thailand', 30, 2, 2643, 43, 36495)
 standard_data[87,] <- list('2020-04-16', 'Thailand', 29, 3, 2672, 46, 37462)
+standard_data[88,] <- list('2020-04-17', 'Thailand', 28, 1, 2700, 47, 38670)
+standard_data[89,] <- list('2020-04-18', 'Thailand', 33, 0, 2733, 47, 38670)
+standard_data[90,] <- list('2020-04-19', 'Thailand', 32, 0, 2765, 47, 39511)
+
 
 
 # Re-create Changes, Growth_Factor, Growth_Rate
