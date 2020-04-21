5f12c8c44d239a6f31644389e1274ba13ff44fd5
paulapivat <paul.apivat@gmail.com>
2020-04-21 14:52:56 +0700

    add april 20 and 21 data

--- daily_data/R/analysis.R
+++ daily_data/R/analysis.R
@@ -237,8 +237,8 @@ standard_data[87,] <- list('2020-04-16', 'Thailand', 29, 3, 2672, 46, 37462)
 standard_data[88,] <- list('2020-04-17', 'Thailand', 28, 1, 2700, 47, 38670)
 standard_data[89,] <- list('2020-04-18', 'Thailand', 33, 0, 2733, 47, 38670)
 standard_data[90,] <- list('2020-04-19', 'Thailand', 32, 0, 2765, 47, 39511)
-
-
+standard_data[91,] <- list('2020-04-20', 'Thailand', 27, 0, 2792, 47, 42257)
+standard_data[92,] <- list('2020-04-21', 'Thailand', 19, 1, 2811, 48, 42302)
 
 # Re-create Changes, Growth_Factor, Growth_Rate
 ddc_who_data <- ddc_who_data %>% arrange(date) %>% mutate(Changes = total_cases - lag(total_cases, default = first(total_cases)))
