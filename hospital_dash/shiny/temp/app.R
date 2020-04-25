fe9ab91c046adfd1d3d9d34a3428c6e7cee896c1
paulapivat <paul.apivat@gmail.com>
2020-04-25 15:00:57 +0700

    add modal to temp

--- hospital_dash/shiny/temp/app.R
+++ hospital_dash/shiny/temp/app.R
@@ -1,24 +1,44 @@
 library(shiny)
 library(shinyjs)
 
-ui <- fluidPage(
-  useShinyjs(),
-  #navbarPage("",
-             sidebarLayout(position = "right",
-                      div( id ="Sidebar",sidebarPanel(
-                      )),
+if (interactive()){
+  shinyApp(
+    
+    ui <- fluidPage(
+      
+      actionButton("show", "Show modal dialog"),
+      
+      useShinyjs(),
+      sidebarLayout(position = "right",
+                    div( id ="Sidebar",sidebarPanel(
+                    )),
+                    
+                    
+                    mainPanel(actionButton("toggleSidebar", "Toggle sidebar")
+                    )
+      )
+    ), # ui
+    
+    server <-function(input, output, session) {
+      observeEvent(input$toggleSidebar, {
+        shinyjs::toggle(id = "Sidebar")
+      })
+      
+      observeEvent(input$show, {
+        url <- a("Explainer Post", href="http://getwyze.com/")
+        showModal(modalDialog(
+          title = "Important Message",
+          tagList("This is an important message!", url)
+        ))
+      })
+      
+    }
+    
+  ) # shinyApp
+} # if (interactive())
 
 
-                      mainPanel(actionButton("toggleSidebar", "Toggle sidebar")
-                      )
-             )
-  #)
-)
 
-server <-function(input, output, session) {
-  observeEvent(input$toggleSidebar, {
-    shinyjs::toggle(id = "Sidebar")
-  })
-}
+
 
 shinyApp(ui, server)
