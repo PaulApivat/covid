f3d59dbca6d7a14122a70cc6a8759dbe42b57e3f
paulapivat <paul.apivat@gmail.com>
2020-05-04 15:33:38 +0700

    complete basic timeline api for ddc

--- ddc_api/R/analysis.R
+++ ddc_api/R/analysis.R
@@ -1,7 +1,9 @@
 # R version 3.6.3 (2020-02-29) -- "Holding the Windsock"
 # RStudio Version 1.2.5042
 
-##### DDC API ######
+##### INTERACT WITH DDC API ######
+
+## NOTE: below is simple API request; some APIs require user-info
 
 # packages and libraries
 install.packages(c('httr', 'jsonlite', 'tidyverse'))
@@ -15,11 +17,25 @@ timeline = GET('https://covid19.th-stat.com/api/open/timeline')
 today = GET('https://covid19.th-stat.com/api/open/today')
 
 # Check for 'Status: 200' success
+# Also View(timeline), View(today) to get feel for data structure (e.g., url, content, date etc)
 
 # Response [https://covid19.th-stat.com/api/open/timeline]
   # Date: 2020-05-04 07:49
   # Status: 200
   # Content-Type: application/json
   # Size: 19.4 kB
+
+# Convert raw Unicode into character vector (note: vector of length [1])
+# note this is JSON in character format (still not usable)
+rawToChar(timeline$content)
+rawToChar(today$content)
+
+# From Character Vector, convert to list data structure using fromJSON() in jsonlite library
+timeline_data = fromJSON(rawToChar(timeline$content))
+today_data = fromJSON(rawToChar(today$content))
+
+# for timeline_data, we're interested in $Data dataframe
+View(timeline_data$Data)
+
 
 
