a2d869b035c8b8757fba3c1b758266f7638ab004
paulapivat <paul.apivat@gmail.com>
2020-05-04 16:15:43 +0700

    add cases_sum_data

--- ddc_api/R/analysis.R
+++ ddc_api/R/analysis.R
@@ -3,7 +3,7 @@
 
 ##### INTERACT WITH DDC API ######
 
-## NOTE: below is simple API request; some APIs require user-info
+## NOTE: below is simple API request; some APIs require user-info - password, username
 
 # packages and libraries
 install.packages(c('httr', 'jsonlite', 'tidyverse'))
@@ -16,6 +16,7 @@ library(tidyverse)
 timeline = GET('https://covid19.th-stat.com/api/open/timeline')
 today = GET('https://covid19.th-stat.com/api/open/today')
 cases = GET('https://covid19.th-stat.com/api/open/cases')
+cases_sum = GET('https://covid19.th-stat.com/api/open/cases/sum')
 
 # Check for 'Status: 200' success
 # Also View(timeline), View(today) to get feel for data structure (e.g., url, content, date etc)
@@ -31,15 +32,20 @@ cases = GET('https://covid19.th-stat.com/api/open/cases')
 rawToChar(timeline$content)
 rawToChar(today$content)
 rawToChar(cases$content)
+rawToChar(cases_sum$content)
 
 # From Character Vector, convert to list data structure using fromJSON() in jsonlite library
 timeline_data = fromJSON(rawToChar(timeline$content))
 today_data = fromJSON(rawToChar(today$content))
 cases_data = fromJSON(rawToChar(cases$content))
-
+cases_sum_data = fromJSON(rawToChar(cases_sum$content))
 
 # for timeline_data, cases_data, we're interested in $Data dataframe
+# This is dataframe format ready for further analysis in R
+# Note: still need to View()
 View(timeline_data$Data)
 View(cases_data$Data)
 
+View(cases_sum_data$Province)
 
+## NOTE cases_sum_data is nested list, so conversion to data.frame requires inspection of several lists.
